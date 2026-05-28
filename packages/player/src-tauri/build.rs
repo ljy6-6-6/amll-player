fa08fe6c9690dfa8b65768d66e1b9eeb677fd1e1
apoint123 <108002475+apoint123@users.noreply.github.com>
2026-05-28 18:30:56 +0800

    fix: 移除命令调用权限限制

--- packages/player/src-tauri/build.rs
+++ packages/player/src-tauri/build.rs
@@ -1,20 +1,3 @@
 fn main() {
-    let attrs =
-        tauri_build::Attributes::new().app_manifest(tauri_build::AppManifest::new().commands(&[
-            "extension_window_create",
-            "extension_window_get",
-            "extension_window_close",
-            "extension_window_close_all",
-            "extension_window_show",
-            "extension_window_hide",
-            "extension_window_focus",
-            "extension_window_center",
-            "extension_window_set_title",
-            "extension_window_set_size",
-            "extension_window_set_position",
-            "extension_window_mark_ready",
-            "extension_window_get_current",
-            "extension_window_get_current_extension_files",
-        ]));
-    tauri_build::try_build(attrs).expect("failed to run tauri build script");
+    tauri_build::build();
 }
